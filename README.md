d371eba500d8c36d4fc6cbfe80b70f634fc5f367
rinaschedl <90951435+rinaschedl@users.noreply.github.com>
2025-12-17 08:17:54 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Diese Seite bei [https://rinaschedl.github.io/hot-balloon-4ab/](https://rinaschedl.github.io/hot-balloon-4ab/) öffnen
+
+## Als Erweiterung verwenden
+
+Dieses Repository kann als **Erweiterung** in MakeCode hinzugefügt werden.
+
+* öffne [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* klicke auf **Neues Projekt**
+* klicke auf **Erweiterungen** unter dem Zahnrad-Menü
+* nach **https://github.com/rinaschedl/hot-balloon-4ab** suchen und importieren
+
+## Dieses Projekt bearbeiten
+
+Um dieses Repository in MakeCode zu bearbeiten.
+
+* öffne [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* klicke auf **Importieren** und dann auf **Importiere URL**
+* füge **https://github.com/rinaschedl/hot-balloon-4ab** ein und klicke auf Importieren
+
+#### Metadaten (verwendet für Suche, Rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,53 @@
+controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
+    mySprite.top += -1
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.top += -1
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`meinBild`, SpriteKind.Player)
+mySprite.setStayInScreen(true)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+let gegner = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 2 2 2 . . . . . . . 
+    . . . . . . 2 2 2 . . . . . . . 
+    . . . . . . 2 2 2 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)
+gegner.setVelocity(10, 0)
+gegner.setPosition(4, 59)
+gegner.x = 10
+forever(function () {
+    mySprite.y += 1
+    pause(100)
+    mySprite.y += 1
+})
